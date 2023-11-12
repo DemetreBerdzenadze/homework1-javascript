503d61bf7543e55a3c890d514d55dab49dd5cb8b
DemetreBerdzenadze <demeberdzenadze@gmail.com>
2023-11-12 16:50:14 +0400

    Fininshed

---
+++ script.js
@@ -0,0 +1,42 @@
+let number = 100;
+
+if (number < 50) {
+  console.log('ნაკლებია 50-ზე');
+} else if (number > 20) {
+  console.log('მეტია 20-ზე')
+} else {
+  console.log(შეცდომა)
+}
+
+
+
+let Name = 'მარიამი';
+
+if (Name == "მარიამი") {
+  console.log('ture')
+} else {
+  console.log("false")
+}
+
+
+
+let Name2 = "მარიამი";
+
+switch (Name2) {
+  case 'მარიამი':
+    console.log("true")
+    break;
+  default:
+    console.log('false')
+    break;
+}
+
+
+let anything = 'ნებისმიერი';
+
+console.log('string')
+
+
+let number2 = 10;
+
+console.log(String(number2));
